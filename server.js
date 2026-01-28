8eb513985a88692ae111183ef323136cf4555eb6
Abhinash Sharma <52877872+Abhinash16@users.noreply.github.com>
2026-01-28 18:28:24 +0530

    fix entry point

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-  })
+  }),
 );
 
 // JSON parser (for normal APIs)
@@ -23,7 +23,7 @@ app.use("/api/order", require("./routes/order.routes"));
 app.use(
   "/api",
   express.raw({ type: "*/*" }),
-  require("./routes/razorpay.routes")
+  require("./routes/razorpay.routes"),
 );
 
 // Health check
