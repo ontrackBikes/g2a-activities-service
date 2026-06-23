aba6331824ead6b1e94848cb0f6e54c7c4f9c6a4
Abhinash Sharma <52877872+Abhinash16@users.noreply.github.com>
2026-06-23 22:56:02 +0530

    Prod Change

--- crons/index.js
+++ crons/index.js
@@ -5,8 +5,16 @@ const mediaCleanupCron = require("./media/mediaCleanup.cron");
 module.exports = () => {
   console.info("[CRON] Registering jobs");
 
-  // TEST ONLY
-  cron.schedule("* * * * *", mediaCleanupCron);
+  // Every 6 hours
+  cron.schedule(
+    "0 */6 * * *",
+    mediaCleanupCron,
+    {
+      timezone: "UTC",
+    }
+  );
 
-  console.info("[CRON] Media cleanup registered");
+  console.info(
+    "[CRON] Media cleanup registered (every 6 hours)"
+  );
 };
