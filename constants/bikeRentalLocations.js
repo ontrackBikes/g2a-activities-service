62d73f2dff1b073c21fc83961d20ea5da329fea5
shilton04 <shiltonr42@gmail.com>
2026-07-22 13:02:59 +0530

    feat: add new bike rental locations including Havelock Jetty, Full Moon Cafe, and Neil Kendra Market

--- constants/bikeRentalLocations.js
+++ constants/bikeRentalLocations.js
@@ -43,4 +43,44 @@ module.exports = Object.freeze([
     pickup: true,
     drop: true,
   },
+  {
+    id: 4,
+    name: "Havelock Jetty",
+    slug: "havelock-jetty",
+    location_slug: "havelock-island",
+    type: "Jetty",
+    image:
+      "https://i0.wp.com/go2andaman.com/wp-content/uploads/2025/08/hvl03-e1746531651444-831x468-1.webp?w=831&ssl=1",
+    address: "Havelock no. 3, Andaman and Nicobar Islands 744211",
+    gmapLink: "https://maps.app.goo.gl/XC617bHdACr7NfEJA",
+    pickup: true,
+    drop: true,
+  },
+  {
+    id: 5,
+    name: "Full Moon Cafe",
+    slug: "full-moon-cafe",
+    location_slug: "havelock-island",
+    type: "Outlet",
+    image:
+      "https://i0.wp.com/go2andaman.com/wp-content/uploads/2025/08/2024-12-17.webp?resize=1024%2C576&ssl=1",
+    address: "Havelock no. 3, Andaman and Nicobar Islands 744211",
+    gmapLink: "https://maps.app.goo.gl/n6EAfoXmoobZGrDTA",
+    pickup: true,
+    drop: true,
+  },
+  {
+    id: 6,
+    name: "Neil Kendra Market",
+    slug: "neil-kendra-market",
+    location_slug: "neil-island",
+    type: "Market",
+    image:
+      "https://i0.wp.com/go2andaman.com/wp-content/uploads/2025/08/2024-12-17.webp?resize=1024%2C576&ssl=1",
+    address:
+      "Neil Kendra, Shaheed, Neil Kendra, Andaman and Nicobar Islands 744104",
+    gmapLink: "https://maps.app.goo.gl/mx8BZY9B2ACZXRrJ6",
+    pickup: true,
+    drop: true,
+  },
 ]);
